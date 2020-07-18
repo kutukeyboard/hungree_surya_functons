a779c7c5504a40183cb44fcc22e653fbcd85b4e7
kutukeyboard <kutukeyboard@gmail.com>
2020-07-18 16:05:25 +0700

    implement cors

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@ const functions = require("firebase-functions");
 const express = require("express");
 const router = express.Router();
 const app = express();
+const cors = require("cors");
 
 const productCategory = require("./controllers/productCategory");
 router.use("/productcategory", productCategory);
@@ -12,6 +13,6 @@ router.use("/user", user);
 const product = require("./controllers/product");
 router.use("/product", product);
 
-app.use("/api", router);
+app.use("/api", cors(), router);
 
 exports.app = functions.https.onRequest(app);
