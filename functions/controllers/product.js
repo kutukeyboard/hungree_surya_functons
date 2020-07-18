9f15a11da9fa08477c1115990a6832853d3e3a5f
kutukeyboard <kutukeyboard@gmail.com>
2020-07-18 13:32:36 +0700

    add product endpoint

---
+++ functions/controllers/product.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const router = express.Router();
+const { db } = require("../helpers/firebaseAdmin");
+const verifyToken = require("../helpers/verifyToken");
+
+router.get("/", verifyToken, async (req, res) => {
+	try {
+		const product = await db.collection("products").get();
+		let products = [];
+		product.forEach((doc) => {
+			categories.push(doc.data());
+		});
+		res.json(products);
+	} catch (error) {
+		res.status(400).json(error);
+	}
+});
+
+router.post("/", verifyToken, async (req, res) => {
+	try {
+		const product = await db.collection("products").add(req.body);
+		res.status(201).json({ message: "document added succesfully" });
+	} catch (error) {
+		console.log(error);
+		res.status(500).json(error);
+	}
+});
+
+router.put("/:id", verifyToken, async (req, res) => {
+	try {
+		const product = await db
+			.collection("products")
+			.doc(req.params.id)
+			.set(req.body);
+		res.status(200).json({ message: "document updated succesfully" });
+	} catch (error) {
+		console.log(error);
+		res.status(500).json(error);
+	}
+});
+
+router.delete("/:id", verifyToken, async (req, res) => {
+	try {
+		const product = await db
+			.collection("products")
+			.doc(req.params.id)
+			.set(req.body);
+		res.status(200).json({ message: "document flaged as deleted succesfully" });
+	} catch (error) {
+		console.log(error);
+		res.status(500).json(error);
+	}
+});
+
+module.exports = router;
--- functions/index.js
+++ functions/index.js
@@ -9,6 +9,9 @@ router.use("/productcategory", productCategory);
 const user = require("./controllers/user");
 router.use("/user", user);
 
+const product = require("./controllers/product");
+router.use("/product", product);
+
 app.use("/api", router);
 
 exports.app = functions.https.onRequest(app);
